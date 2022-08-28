b76784cb1b96c28bb8cc4aedc94670c29a72f00b
com0rade1 <protasovnekit@gmail.com>
2022-08-28 20:57:23 +0300

    app uploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BeginButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(18, 125);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1170, 554);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Температура";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вязкость среды";
+            // 
+            // BeginButton
+            // 
+            this.BeginButton.Location = new System.Drawing.Point(766, 37);
+            this.BeginButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BeginButton.Name = "BeginButton";
+            this.BeginButton.Size = new System.Drawing.Size(112, 35);
+            this.BeginButton.TabIndex = 6;
+            this.BeginButton.Text = "Запуск";
+            this.BeginButton.UseVisualStyleBackColor = true;
+            this.BeginButton.Click += new System.EventHandler(this.BeginButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(942, 37);
+            this.StopButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(112, 35);
+            this.StopButton.TabIndex = 7;
+            this.StopButton.Text = "Остановка";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(494, 42);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 26);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 80);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(452, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Заполнить поля случайными значениями";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.BeginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BeginButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        float x0;
+        float y0;
+        int Temperature;
+        int Viscosity;
+        int a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BeginButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.Clear(Color.White);
+            this.Temperature = Convert.ToInt32(textBox1.Text);
+            this.Viscosity = Convert.ToInt32(textBox2.Text);
+            timer1.Interval = 1000-10*this.Temperature;
+            timer1.Enabled = true;
+            timer1.Start();
+            this.x0 = (float)(this.rnd.Next(0, 780));
+            this.y0 = (float)(this.rnd.Next(0, 360));
+
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            this.a = this.rnd.Next(0, 360);
+            float r = (float)(100/(this.Viscosity+1));
+            float x1 = (float)(this.x0 + r * Math.Cos(this.a * (Math.PI) / 180));
+            while (x1>=780 || x1<0)
+            {
+            x1 = (float)(this.x0 + r * Math.Cos(this.a * (Math.PI) / 180));
+                this.a = this.rnd.Next(1, 360);
+                r = (float)(this.rnd.Next(1, 100)/(this.Viscosity + 1));
+            }
+            float y1 = (float)(this.y0 + r * Math.Sin(this.a * (Math.PI) / 180));
+            while (y1>=360 || y1<0)
+            {
+                y1 = (float)(this.y0 + r * Math.Sin(this.a * (Math.PI) / 180));
+                this.a = this.rnd.Next(1, 360);
+                r = (float)(this.rnd.Next(1, 100)/(this.Viscosity + 1));
+            }
+            g.DrawLine(new Pen(Color.Black), this.x0, this.y0, x1, y1);
+            this.x0 = x1;
+            this.y0 = y1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(rnd.Next(0, 100));
+            textBox2.Text = Convert.ToString(rnd.Next(0, 10));
+            this.BeginButton_Click(new object(), new EventArgs());
+        }
+    }
+}
